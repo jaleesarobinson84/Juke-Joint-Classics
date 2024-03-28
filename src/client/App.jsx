cb7a7b1b9d13627351d60b47f6667eb61cb6ba2a
Mandell1981 <Mandell1981.rm@gmail.com>
2024-03-28 11:40:36 -0400

    added mongoose

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+
+
+
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
+    
     <div className="App">
+      <navbar>
+        
+      </navbar>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -15,17 +23,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Juke Joint Classics</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.kCM-JMMQ265y2zElmG7mlwHaFj&pid=Api&P=0&h=220"/>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Feeding your Soul with these Southern Home Cooking Recipes!
       </p>
     </div>
   );
