532a0a2a8913d6aac8d99e3147e68a5cf0784dcf
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-04-01 12:27:43 -0400

    merge conflicts are completed

--- src/server/main.js
+++ src/server/main.js
@@ -1,11 +1,18 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from 'mongoose';
+
 
 
 
 // Configurations
 // require('dotenv').config();
 const app = express();
+// // mongoose
+// const mongoose = require('mongoose');
+// Connection URL
+const uri = 'mongodb://localhost:27017/mydatabase';
+
 
 // Middleware
 
@@ -20,3 +27,13 @@ app.get("", (req, res) => {
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Continue with your application logic here
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
