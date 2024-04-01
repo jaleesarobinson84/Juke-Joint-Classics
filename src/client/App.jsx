811a636336e9b9cf236feac2e117d081e0f5ab52
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-04-01 12:05:45 -0400

    removed mongodb import from main.js, and removed function app on server/app.jsx to correct conflict merge errors

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 
-function App() {
-  const [count, setCount] = useState(0);
 
-}
 
 export default App;
--- src/server/main.js
+++ src/server/main.js
@@ -1,6 +1,5 @@
 import express from "express";
 import ViteExpress from "vite-express";
-// import mongoose from 'mongoose';
 
 
 
